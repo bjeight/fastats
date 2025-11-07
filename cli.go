77ac84cae8feb5f135900e98b5c1eb88b46d42b5
benjamincjackson <benjamincjackson@gmail.com>
2025-11-07 16:00:35 +0000

    Add biogo licence

--- cli.go
+++ cli.go
@@ -15,6 +15,12 @@ var (
 		Long:              ``,
 		Version:           "0.12.0",
 		CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
+		Run: func(cmd *cobra.Command, args []string) {
+			if licences {
+				printLicences()
+				os.Exit(0)
+			}
+		},
 	}
 )
 
@@ -26,6 +32,8 @@ func main() {
 }
 
 var (
+	licences bool
+
 	f bool
 	c bool
 	d bool
@@ -50,6 +58,37 @@ var (
 	out io.Writer = os.Stdout
 )
 
+func printLicences() {
+	os.Stdout.WriteString(`fastats is unlicensed software. All rights reserved.
+	
+fastats contains some of bíogo, which is distributed under the BSD-3-Clause license:
+	
+Copyright ©2012 The bíogo Authors. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the bíogo project nor the names of its authors and
+      contributors may be used to endorse or promote products derived from this
+      software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+`)
+}
+
 func getFileBoolVarPArgs() (*bool, string, string, bool, string) {
 	return &f, "file", "f", false, "calculate statistics per file (default is per record)"
 }
@@ -71,6 +110,9 @@ func getInverseBoolVarPArgs() (*bool, string, string, bool, string) {
 }
 
 func init() {
+
+	rootCmd.Flags().BoolVarP(&licences, "licences", "l", false, "print licence information and exit")
+	rootCmd.Flags().Lookup("licences").NoOptDefVal = "true"
 
 	rootCmd.AddCommand(contentCmd)
 	rootCmd.AddCommand(atCmd)
